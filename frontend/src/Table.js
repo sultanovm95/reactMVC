aaa0c40ebf4c7a74f2aa58f34a3e73e994063759
Muhammadorif Sultanov <sultanovm95@gmail.com>
2022-01-26 23:45:34 -0800

    Adding v1.0 MVC architecture

---
+++ frontend/src/Table.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function TableBody (props) {
+	const rows = props.characterData.map((row, index) => {
+	  return (
+		<tr key={index}>
+			<td>{row.name}</td>
+			<td>{row.job}</td>
+			<td>{row._id}</td>
+			<td>
+				<button onClick={() => props.removeCharacter(row._id)}>Delete</button>
+			</td>
+		</tr>
+	  );
+	});
+	
+	return (
+		<tbody>
+		   {rows}
+		</tbody>
+	 );
+}
+
+function TableHeader() {
+	return (
+	  <thead>
+		<tr>
+		  <th>Name</th>
+		  <th>Job</th>
+		  <th>ID</th>
+		  <th>Remove</th>
+		</tr>
+	  </thead>
+	);
+}
+
+
+function Table(props) {
+	return (
+	  <table>
+		<TableHeader />
+		<TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+	  </table>
+	);
+}
+
+export default Table;
---
+++ frontend/src/index.js
@@ -0,0 +1,93 @@
+import  {React, useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Table from './Table';
+import Form from './form';
+import './index.css';
+
+
+function MyApp() {
+
+  const [characters, setCharacters] = useState([]);
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get('http://localhost:3030/users');
+      console.log(response.data.data.allUsers);
+      return response.data.data.allUsers;
+    }
+    catch(error) {
+      // We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if(result) {
+        setCharacters(result);
+      }
+    });
+  }, []);
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post('http://localhost:3030/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  function updateList(person) {
+    makePostCall(person).then( result => {
+      if(result.status === 201) {
+        console.log(result.data.result);
+        setCharacters([...characters, result.data.result]);
+      }
+    });
+  }
+
+  function deleteById(id) {
+    makeDeleteCall(id).then( result => {
+      if(result.status === 200) {
+        console.log(result);
+        const updated = characters.filter((character) => {
+          if(result.data === character._id) {
+            return false;
+          }
+          return true;
+        });
+        setCharacters(updated);
+      }
+    });
+  }
+
+
+  async function makeDeleteCall (id) {
+    console.log(id);
+    try {
+      const response = await axios.delete('http://localhost:3030/users/'+id);
+      console.log("IN waiting: " + response);
+      return response;
+    } 
+    catch(error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={deleteById} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
+
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
