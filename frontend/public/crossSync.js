d0442bc3faf835a3db565d48115443751fe45b84
Muhammadorif Sultanov <sultanovm95@gmail.com>
2022-01-29 00:08:09 -0800

    Optimized crossSync

--- frontend/public/crossSync.js
+++ frontend/public/crossSync.js
@@ -2,11 +2,13 @@ const CHANNEL_NAME = 'demo';
 const CHECK_INTERVAL = 50;
 
 let previousMessage = window.localStorage.getItem(CHANNEL_NAME);
-document.addEventListener("DOMContentLoaded", () => {
- 
-  const submitSigalButton = document.querySelector('#submitSignal');
-  submitSigalButton.addEventListener('click', sendMessage);
 
+document.addEventListener("DOMContentLoaded", () => {
+  const submitSigalButton = document.querySelector('#submitSignal');
+  submitSigalButton.addEventListener('click', checkForUpadte());
+});
+
+function checkForUpadte() {
   if (window.localStorage) {
     setInterval(() => {
       const message = window.localStorage.getItem(CHANNEL_NAME);
@@ -18,9 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
 
     window.localStorage.removeItem(CHANNEL_NAME);
   }
-
-});
-
-function sendMessage() {
-  window.localStorage.removeItem(CHANNEL_NAME);
 }
